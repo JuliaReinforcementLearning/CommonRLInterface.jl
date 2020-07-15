f2c50e507330c6343a8ea640fe8b7ba09f682be4
Zachary Sunberg <sunbergzach@gmail.com>
2020-07-14 20:10:51 -0600

    added state and setstate!

--- src/environment.jl
+++ src/environment.jl
@@ -15,7 +15,7 @@ Return a showable object that visualizes the environment at the current state.
 Implementing this function will facilitate visualization through the [Julia Multimedia I/O system](https://docs.julialang.org/en/v1/base/io-network/#Multimedia-I/O-1), for example, calling `display(render(env))` will cause the visualization to pop up in a Jupyter notebook, the Juno plot pane, or an ElectronDisplay.jl window. `render` should return an object that has `Base.show` methods for many MIME types. Good examples include a Plots.jl plot, a Compose.jl `Context` graphic produced by Cairo, or a custom type with `show` methods.
 
 # Example
-```
+```julia
 using CommonRLInterface
 using Plots
 
@@ -27,3 +27,21 @@ env
 ```
 """
 function render end
+
+"""
+    state(env::AbstractEnv)
+
+Return the state of the environment.
+
+See setstate! for more information about the state.
+"""
+function state end
+
+"""
+    setstate!(env::AbstractEnv, s)
+
+Set the state of the environment to s.
+
+If two environments have the same state, future outputs (observations and rewards) should be statistically identical given the same sequence of actions.
+"""
+function setstate! end
