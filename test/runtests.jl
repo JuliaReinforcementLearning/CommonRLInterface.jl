5fed1027387095ff54ffb773f458a0f807a8fc22
Zachary Sunberg <sunbergzach@gmail.com>
2020-07-31 10:57:31 -0700

    only run gridworld tests on recent versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -157,4 +157,6 @@ end
     @test observations(env) == 1:10
 end
 
-include("examples/gridworld.jl")
+if VERSION >= v"1.4" # not sure if this is the actual minimal version, but I know it will work
+    include("examples/gridworld.jl")
+end
